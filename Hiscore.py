d97c522a1af6924985d1813cc5f23964a52e362b
dress131 <dress131@d.umn.edu>
2017-02-14 08:26:57 -0600

    Works now with multiple players and outputs to a file

--- Hiscore.py
+++ Hiscore.py
@@ -3,19 +3,26 @@ import requests
 from bs4 import BeautifulSoup
 
 
-page = requests.get('http://services.runescape.com/m=hiscore/index_lite.ws?player=XXXXXXXX')
-data = page.text
-soup = BeautifulSoup(data, "lxml")
-stats = str(soup.p.extract())
-stats = stats.strip("<p>").strip("</p>")
-statList = stats.split()
-x = 1
-total = 0
-#1 == Attack, 2 == Defence, 3 == Strength, 4 == Constitution, 5 == Ranged, 7 == Magic
-while x < 8:
-	if x != 6:
-		rank, level, xp = statList[x].split(',')
-		total += int(xp)
-	x+=1
+f = open('Players.txt', 'r')
+out = open('Initial.txt', 'w')
+playerList = f.read().split()
 
-print(total)
+for name in playerList:
+	page = requests.get('http://services.runescape.com/m=hiscore/index_lite.ws?player=' + name)
+	data = page.text
+	soup = BeautifulSoup(data, "lxml")
+	stats = str(soup.p.extract())
+	stats = stats.strip("<p>").strip("</p>")
+	statList = stats.split()
+	x = 1
+	total = 0
+	#1 == Attack, 2 == Defence, 3 == Strength, 4 == Constitution, 5 == Ranged, 7 == Magic
+	while x < 8:
+		if x != 6:
+			rank, level, xp = statList[x].split(',')
+			total += int(xp)
+		x+=1
+
+	out.write(name + " " + str(total) + "\n")
+f.close()
+out.close()
