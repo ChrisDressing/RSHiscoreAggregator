b3c4f4916562fc90eb9c233ea097a6356362000a
Chris Dressen <cmdressen@gmail.com>
2017-02-15 12:26:56 -0600

    Updated some filepaths as well as made it sort

--- Update.py
+++ Update.py
@@ -1,29 +1,40 @@
+
 from lxml import html
 import requests
 from bs4 import BeautifulSoup
 
-readIn = open('Initial.txt', 'r')
+readIn = open('/home/pi/RSHiscoreAggregator/Initial.txt', 'r')
 readOut = open('/var/www/html/index.html', 'w')
 data = readIn.read().strip()
 playerData = data.split(" ")
 total = -1
+totalxp = []
+playerNames = []
 for line in playerData:
-	playerName, xp = line.split(",")
-	print(playerName + " " + xp)
-	page = requests.get('http://services.runescape.com/m=hiscore/index_lite.ws?player=' + playerName)
-	data = page.text
-	soup = BeautifulSoup(data, "lxml")
-	stats = str(soup.p.extract())
-	stats = stats.strip("<p>").strip("</p>")
-	statList = stats.split()
-	x = 1
-	total = 0
+        playerName, xp = line.split(",")
+        playerNames.append(playerName)
+        page = requests.get('http://services.runescape.com/m=hiscore/index_lite.ws?player=' + playerName)
+        data = page.text
+        soup = BeautifulSoup(data, "lxml")
+        stats = str(soup.p.extract())
+        stats = stats.strip("<p>").strip("</p>")
+        statList = stats.split()
+        x = 1
+        total = 0
 	#1 == Attack, 2 == Defence, 3 == Strength, 4 == Constitution, 5 == Ranged, 7 == Magic
-	while x < 8:
-		if x != 6:
-			rank, level, curXP = statList[x].split(',')
-			total += int(curXP)
-		x+=1
-	total = total - int(xp)
-	print(playerName + " " + str(total))
-	readOut.write(playerName + "," + str(total) + "<br>")
+        while x < 8:
+                if x != 6:
+                        rank, level, curXP = statList[x].split(',')
+                        total += int(curXP)
+                x+=1
+        total = total - int(xp)
+        totalxp.append(total)
+        print(playerName + " " + str(total))
+	#readOut.write(playerName + "," + str(total) + "<br>")
+totalxp, playerNames =  zip(*sorted(zip(totalxp, playerNames)))
+temp = len(totalxp)-1
+rank = 1
+while temp >= 0:
+        readOut.write(str(rank) + ". " + playerNames[temp] + " - " + str(totalxp[temp]) + "<br>")
+        temp-=1
+        rank +=1
