def8846d601a0dd8b6a45d80965ce905d4162d24
Chris Dressen <cmdressen@gmail.com>
2017-02-15 18:26:47 -0600

    added more users

--- Hiscore.py
+++ Hiscore.py
@@ -8,21 +8,22 @@ out = open('Initial.txt', 'w')
 playerList = f.read().split()
 
 for name in playerList:
-	page = requests.get('http://services.runescape.com/m=hiscore/index_lite.ws?player=' + name)
-	data = page.text
-	soup = BeautifulSoup(data, "lxml")
-	stats = str(soup.p.extract())
-	stats = stats.strip("<p>").strip("</p>")
-	statList = stats.split()
-	x = 1
-	total = 0
+        print(name)
+        page = requests.get('http://services.runescape.com/m=hiscore/index_lite.ws?player=' + name)
+        data = page.text
+        soup = BeautifulSoup(data, "lxml")
+        stats = str(soup.p.extract())
+        stats = stats.strip("<p>").strip("</p>")
+        statList = stats.split()
+        x = 1
+        total = 0
 	#1 == Attack, 2 == Defence, 3 == Strength, 4 == Constitution, 5 == Ranged, 7 == Magic
-	while x < 8:
-		if x != 6:
-			rank, level, xp = statList[x].split(',')
-			total += int(xp)
-		x+=1
+        while x < 8:
+                if x != 6:
+                        rank, level, xp = statList[x].split(',')
+                        total += int(xp)
+                x+=1
 
-	out.write(name + "," + str(total) + " ")
+        out.write(name + "," + str(total) + " ")
 f.close()
 out.close()
--- Update.py
+++ Update.py
@@ -39,5 +39,5 @@ while temp >= 0:
         html+= str(rank) + ". " + playerNames[temp] + " - " + str(totalxp[temp]) + "<br>"
         temp-=1
         rank +=1
-html+="</p></body></html>"
+html+="</p><br><i><b>Note: This tracker updates once every 15 minutes or so, if it's not, please let Chris D know</b></i></body></html>"
 readOut.write(html)
