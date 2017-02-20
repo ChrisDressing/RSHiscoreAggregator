f983ede36473b8e8112d7e18638f76b35189e2f6
Chris Dressen <cmdressen@gmail.com>
2017-02-20 04:17:36 -0600

    Committed for posterity

--- Update.py
+++ Update.py
@@ -11,26 +11,30 @@ total = -1
 totalxp = []
 playerNames = []
 for line in playerData:
-        playerName, xp = line.split(",")
-        playerNames.append(playerName)
-        page = requests.get('http://services.runescape.com/m=hiscore/index_lite.ws?player=' + playerName)
-        data = page.text
-        soup = BeautifulSoup(data, "lxml")
-        stats = str(soup.p.extract())
-        stats = stats.strip("<p>").strip("</p>")
-        statList = stats.split()
-        x = 1
-        total = 0
-	#1 == Attack, 2 == Defence, 3 == Strength, 4 == Constitution, 5 == Ranged, 7 == Magic
-        while x < 8:
-                if x != 6:
-                        rank, level, curXP = statList[x].split(',')
-                        total += int(curXP)
-                x+=1
-        total = total - int(xp)
-        totalxp.append(total)
-        print(playerName + " " + str(total))
-	#readOut.write(playerName + "," + str(total) + "<br>")
+        try:
+                playerName, xp = line.split(",")
+                playerNames.append(playerName)
+                page = requests.get('http://services.runescape.com/m=hiscore/index_lite.ws?player=' + playerName)
+                data = page.text
+                soup = BeautifulSoup(data, "lxml")
+                stats = str(soup.p.extract())
+                stats = stats.strip("<p>").strip("</p>")
+                statList = stats.split()
+                x = 1
+                total = 0
+                #1 == Attack, 2 == Defence, 3 == Strength, 4 == Constitution, 5 == Ranged, 7 == Magic
+                while x < 8:
+                        if x != 6:
+                                rank, level, curXP = statList[x].split(',')
+                                total += int(curXP)
+                        x+=1
+                total = total - int(xp)
+                totalxp.append(total)
+                print(playerName + " " + str(total))
+                #readOut.write(playerName + "," + str(total) + "<br>")
+        except:
+                print("There was an error with a user")
+                
 totalxp, playerNames =  zip(*sorted(zip(totalxp, playerNames)))
 temp = len(totalxp)-1
 rank = 1
