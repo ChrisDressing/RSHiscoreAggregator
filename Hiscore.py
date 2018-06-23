ff97a034e6b7769b82ae5222e2ada91c909d8ba8
Chris Dressen <cmdressen@gmail.com>
2018-06-23 00:55:24 +0000

    Updating after modifications

---
+++ Hiscore.py
@@ -0,0 +1,68 @@
+from lxml import html
+import requests
+from bs4 import BeautifulSoup
+import time
+
+f = open('/root/RSHiscoreAggregator/Players.txt', 'r')
+out = open('/root/RSHiscoreAggregator/Initial.txt', 'w')
+playerList = f.read().split()
+
+for name in playerList:
+    print(name)
+    page = requests.get('http://services.runescape.com/m=hiscore/index_lite.ws?player=' + name)
+    data = page.text
+    soup = BeautifulSoup(data, "lxml")
+    stats = str(soup.p.extract())
+    stats = stats.strip("<p>").strip("</p>")
+    statList = stats.split()
+    x = 0
+    total = 0
+    # 1 == Attack, 2 == Defence, 3 == Strength, 4 == Constitution, 5 == Ranged, 7 == Magic
+    disq = False
+    attempts = 0
+    try:
+        while x < 27:
+            if x == 15:  # or x == 19:
+                rank, level, xp = statList[x].split(',')
+                print(xp)
+                while int(xp) <= 0 and attempts < 3:
+                    attempts += 1
+                    time.sleep(1)
+                    page = requests.get('http://services.runescape.com/m=hiscore/index_lite.ws?player=' + name)
+                    data = page.text
+                    soup = BeautifulSoup(data, "lxml")
+                    stats = str(soup.p.extract())
+                    stats = stats.strip("<p>").strip("</p>")
+                    statList = stats.split()
+                    rank, level, xp = statList[x].split(',')
+                    print(xp)
+                total += int(xp)
+            x += 1
+    except:
+        print("There was a problem with the current player " + name + "... Trying again")
+        disq = True
+    if disq is True:
+        out.write(name + "," + "D" + " ")
+        disq = False
+    else:
+        out.write(name + "," + str(total) + " ")
+f.close()
+out.close()
+'''
+Overall 0      | Herblore 16
+Attack  1      | Agility 17
+Defence 2      | Thieving 18
+Strength 3     | Slayer 19
+Constitution 4 | Farming 20
+Ranged 5       | Runecrafting 21
+Prayer 6       | Hunter 22
+Magic 7        | Construction 23
+Cooking 8      | Summoning 24
+Woodcutting 9  | Dungeoneering 25
+Fletching 10   | Divination 26
+Fishing 11     | Invention 27
+Firemaking 12  |
+Crafting 13    |
+Smithing 14    |
+Mining 15      | 
+'''
---
+++ Members.py
@@ -0,0 +1,27 @@
+import fileinput
+import requests
+
+#The URL to get the guild members is http://services.runescape.com/m=clan-hiscores/members_lite.ws?clanName=casual+escape
+fw = open('/root/RSHiscoreAggregator/members_lite.ws','w',encoding = "ISO-8859-1")
+page = requests.get('http://services.runescape.com/m=clan-hiscores/members_lite.ws?clanName=casual+oasis');
+fw.write(page.text)
+fw.close()
+f = open('/root/RSHiscoreAggregator/members_lite.ws', encoding = "ISO-8859-1")
+out = open('Players.txt', 'w')
+#playerList = f.read().replace(" ", "_").split()
+temp = 0
+g = ""
+with fileinput.input('/root/RSHiscoreAggregator/members_lite.ws', openhook=fileinput.hook_encoded("iso-8859-1")) as fileT:
+    for line in fileT:
+        if temp != 0:
+            tempLine = line.replace(" ", "_")
+            name = line.split(",")
+            tempName = name[0].replace(" ","_").replace("\xa0","_")
+            print(tempName)
+            g+=tempName+"\n"
+        else:
+            temp=1
+        #print(fileinput.filename())
+#g+="MadClikr\n"
+out.write(g)
+out.close()
